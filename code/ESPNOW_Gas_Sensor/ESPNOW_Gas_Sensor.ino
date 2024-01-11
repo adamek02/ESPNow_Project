1698bc4b310f38c096098e619ff553f24d523ff1
adamek02 <79096598+adamek02@users.noreply.github.com>
2024-01-11 23:05:30 +0100

    Add files via upload

--- code/ESPNOW_Gas_Sensor/ESPNOW_Gas_Sensor.ino
+++ code/ESPNOW_Gas_Sensor/ESPNOW_Gas_Sensor.ino
@@ -1,16 +1,21 @@
 #include <WiFi.h>
 #include <esp_now.h>
 
+#define uS_TO_S_FACTOR 1000000  /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP  30       /* Time ESP32 will go to sleep (in seconds) */
 
 char Dev_name[8] = "GAS";
 char Dev_name_2[8] = "GAS_t_s";
 
 uint8_t broadcastAddress[] = {0x3C, 0x71, 0xBF, 0x4D, 0x79, 0x34};
 
+unsigned long sleep_time = 0;
+unsigned long last_sleep_time = 0;
+int awake_time = 5000;
 int last_millis = 0;
 int start_millis = 0;
 unsigned long lastTime = 0;  
-unsigned long timerDelay = 10000;  
+unsigned long timerDelay = 1000;  
 unsigned long lastTime_trig = 0;
 unsigned long timerDelay_trig = 1000;
 unsigned long button_time = 0;  
@@ -19,6 +24,8 @@ int trigger = 0;
 int PIN_STATUS = 0;
 int TRIG = 0;
 int gas_trigger;
+
+
 
 typedef struct struct_message {
   char name[8];
@@ -53,12 +60,35 @@ void IRAM_ATTR isr() {
 	gas_sensor.detected = 1;
 }
 
+void print_wakeup_reason(){
+  esp_sleep_wakeup_cause_t wakeup_reason;
+
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+
+  switch(wakeup_reason)
+
+  {case ESP_SLEEP_WAKEUP_EXT0 : 
+
+  TRIG = 1;
+  trigger = 1;
+	gas_sensor.detected = 1;
+  break;
+
+   case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
+  default : Serial.printf("Wakeup was not caused by deep sleep: %d\n",wakeup_reason); break;
+  }
+}
 
 void setup() {
   Serial.begin(115200);
 
   pinMode(gas_sensor.PIN, INPUT_PULLUP);
   attachInterrupt(gas_sensor.PIN, isr, FALLING);
+
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  esp_sleep_enable_ext0_wakeup(GPIO_NUM_25,0);
+
+  print_wakeup_reason();
 
   WiFi.mode(WIFI_STA);
 
@@ -88,29 +118,29 @@ void loop() {
     gas_sensor.detected = 0;
     TRIG = 0;
   }
- 
 
 
   if(gas_sensor.detected == 0 && PIN_STATUS == 1){
     gas_trigger = 0;
     if ((millis() - lastTime) > timerDelay){
 
+
     strcpy(myData.name, Dev_name);
     myData.data = gas_trigger;
     esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
 
-    if(gas_sensor.time > 1){
-        strcpy(myData.name, Dev_name_2);
-        myData.data = gas_sensor.time;
-        esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
-        gas_sensor.time = 0;
-    }
+    // if(gas_sensor.time > 1){
+    //     strcpy(myData.name, Dev_name_2);
+    //     myData.data = gas_sensor.time;
+    //     esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+    //     gas_sensor.time = 0;
+    // }
 
 //
     Serial.print(" Petla 1  ");
     Serial.print(PIN_STATUS);
     Serial.print("   ");
-    Serial.print(gas_sensor.detected);
+    Serial.print( gas_sensor.time);
     Serial.print("   ");
     Serial.print(" Times triggered 0:  ");
     Serial.print(trigger);
@@ -137,7 +167,7 @@ void loop() {
     Serial.print(" Petla 2  ");
     Serial.print(PIN_STATUS);
     Serial.print("   ");
-    Serial.print(gas_sensor.detected);
+    Serial.print( gas_sensor.time);
     Serial.print("   ");
     Serial.print(" Times triggered 1:  ");
     Serial.print(trigger);
@@ -145,6 +175,24 @@ void loop() {
 //
       lastTime_trig = millis();
     }
-    
+  
+}
 
-}}
+ if((gas_sensor.time > 1) && PIN_STATUS == 1){
+        strcpy(myData.name, Dev_name_2);
+        myData.data = gas_sensor.time;
+        esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+        gas_sensor.time = 0;
+        delay(100);
+        }
+
+
+sleep_time = millis();
+if ((sleep_time - last_sleep_time > awake_time) && PIN_STATUS == 1)
+{
+  last_sleep_time = sleep_time;
+  esp_deep_sleep_start();
+}
+
+
+}
